7e9ea55f392d317edc38a497c7b4f1c0a6ccd562
anass bouketaya <bouketayaanass@gmail.com>
2023-01-20 14:02:00 +0100

    use black and remove commented out code

--- src_code/preprocessing.py
+++ src_code/preprocessing.py
@@ -1,88 +1,75 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage import rotate 
+from scipy.ndimage import rotate
 
 
 def binarize(image):
     """This function binarizes  an image using the otsu algorithm
     and returns the original image and the binarized image"""
 
-    #Can we make the binarization better? --> for later
-    #the 0 transforms autmatically the picture to grayscale
-    
-    ret,thresh = cv.threshold(image,127,255,cv.THRESH_OTSU)
-    #imgf = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
-    return  thresh
+    # Can we make the binarization better? --> for later
+    # the 0 transforms autmatically the picture to grayscale
+
+    ret, thresh = cv.threshold(image, 127, 255, cv.THRESH_OTSU)
+    return thresh
 
 
 def find_score(arr, angle):
     data = rotate(arr, angle, reshape=False, order=0)
     hist = np.sum(data, axis=1)
     score = np.sum((hist - np.mean(hist)) ** 2)
-    return  score
+    return score
 
 
 def correct_skew(image):
     """this function corrects skewness using the profile projection method
     it takes as argument the image after binarization"""
 
-    # the binary_image array only contains 0 and 1, to easily calculate the histogramn ktb elach l inversed
-    inversed_binary_image = 1- image//255
-    angles = np.arange(-3,3,0.1)
+    # the binary_image array only contains 0 and 1, to easily calculate the histogram
+    inversed_binary_image = 1 - image // 255
+    angles = np.arange(-3, 3, 0.1)
     scores = []
     for angle in angles:
         scores.append(find_score(inversed_binary_image, angle))
     best_score = np.max(np.array(scores))
     best_angle = angles[scores.index(best_score)]
     corrected_skew = rotate(image, best_angle, reshape=False, order=0, cval=255)
-    
+
     return corrected_skew
-    
-    
-def plot_images(images_list,images_names=["before","after"]):
+
+
+def plot_images(images_list, images_names=["before", "after"]):
     """this function plots two images"""
 
-    plt.subplot(1,2,1),plt.imshow(images_list[0],'gray',vmin=0,vmax=255)
+    plt.subplot(1, 2, 1), plt.imshow(images_list[0], "gray", vmin=0, vmax=255)
     plt.title(images_names[0])
-    plt.xticks([]),plt.yticks([])
-    plt.subplot(1,2,2),plt.imshow(images_list[1],'gray',vmin=0,vmax=255)
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(1, 2, 2), plt.imshow(images_list[1], "gray", vmin=0, vmax=255)
     plt.title(images_names[1])
-    plt.xticks([]),plt.yticks([])
+    plt.xticks([]), plt.yticks([])
     plt.show()
 
-def save_images(name, images_list,path):
 
-    for i,image in enumerate(images_list):
-        cv.imwrite(path+name+str(i)+".jpg",image)
+def save_images(name, images_list, path):
+
+    for i, image in enumerate(images_list):
+        cv.imwrite(path + name + str(i) + ".jpg", image)
+
 
 def remove_noise(image):
-    #shoudl make it way better!
-    return cv.fastNlMeansDenoising(image) 
+    # shoud make it way better!
+    return cv.fastNlMeansDenoising(image)
+
 
 def preprocess(image):
     """this function binarizes, corrects skew and remove noise from the given
     picture"""
 
-    img = cv.imread(image,0)
+    img = cv.imread(image, 0)
     binarized = binarize(img)
     corrected_skew = correct_skew(binarized)
-    #images = [binarized, corrected_skew]
-    #plot_images(images)
     removed_noise = remove_noise(corrected_skew)
-    cv.imwrite("c:/Users/ab/Desktop/src code/lines/binarized.jpg",removed_noise)
+    cv.imwrite("c:/Users/ab/Desktop/src code/lines/binarized.jpg", removed_noise)
 
     return img, removed_noise
-
-#og, final = preprocess("skewed_2.png")
-
-"""def thin_image(image):"""
-"""ths function takes a bnary image as argument and returns
-the image after performing thining and skeletonization""""""
-
-kernel = np.ones((5,5),np.uint8)
-erosion = cv.erode(image,kernel,iterations = 1)
-return erosion"""
-
-
-
--- src_code/segmentation.py
+++ src_code/segmentation.py
@@ -3,50 +3,65 @@ import preprocessing as pre
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-PROJECTION_DICT = { "vertical": 0,"horizontal": 1}
+PROJECTION_DICT = {"vertical": 0, "horizontal": 1}
+
 
 def projection(image, axis):
 
-    ones_and_zeros = 1 - image//255
-    projection_bins = np.sum(ones_and_zeros,axis=PROJECTION_DICT[axis])
+    ones_and_zeros = 1 - image // 255
+    projection_bins = np.sum(ones_and_zeros, axis=PROJECTION_DICT[axis])
     return projection_bins
 
-def segment(image, axis, thresh = 14, cut = 4):
+
+def segment(image, axis, thresh=14, cut=4):
 
     hist = projection(image, axis)
     cnt = 0
     segments = []
     start = -1
-    for idx_2,my_bin in enumerate(hist):
+    for idx_2, my_bin in enumerate(hist):
 
         if my_bin > thresh:
-            cnt=0
+            cnt = 0
 
-        if my_bin > thresh and start==-1:
+        if my_bin > thresh and start == -1:
             start = idx_2
 
-        if my_bin <= thresh and start!=-1:
+        if my_bin <= thresh and start != -1:
 
             cnt += 1
             if cnt >= cut:
 
                 if axis == "horizontal":
-                    segments.append(image[max(start-1,0):idx_2][:])
+                    segments.append(image[max(start - 1, 0) : idx_2][:])
                     start = -1
                     cnt = 0
 
                 elif axis == "vertical":
 
-                    segments.append(image[:,max(start-1,0):idx_2])
+                    segments.append(image[:, max(start - 1, 0) : idx_2])
                     start = -1
                     cnt = 0
 
     return segments
 
-original_img, preprocessed_img = pre.preprocess("c:/Users/ab/Desktop/Anass/isima/OCR-for-arabic-letters/books_for_ocr/scanned_pics/test_2.PNG")
-lines = segment(preprocessed_img,"horizontal")
-for i,line in enumerate(lines):
-    cv.imwrite("c:/Users/ab/Desktop/Anass/isima/OCR-for-arabic-letters/lines/line)"+str(i)+".jpg",line)
+
+original_img, preprocessed_img = pre.preprocess(
+    "c:/Users/ab/Desktop/Anass/isima/OCR-for-arabic-letters/books_for_ocr/scanned_pics/test_2.PNG"
+)
+lines = segment(preprocessed_img, "horizontal")
+for i, line in enumerate(lines):
+    cv.imwrite(
+        "c:/Users/ab/Desktop/Anass/isima/OCR-for-arabic-letters/lines/line)"
+        + str(i)
+        + ".jpg",
+        line,
+    )
 words = segment(lines[0], "vertical", 0, 6)
-for i,word in enumerate(words):
-    cv.imwrite("c:/Users/ab/Desktop/Anass/isima/OCR-for-arabic-letters/words/word)"+str(i)+".jpg",word)    
+for i, word in enumerate(words):
+    cv.imwrite(
+        "c:/Users/ab/Desktop/Anass/isima/OCR-for-arabic-letters/words/word)"
+        + str(i)
+        + ".jpg",
+        word,
+    )
